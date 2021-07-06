dccf234d1276bfc14b080ae3718d3eacf168cbb7
Carlos Costa <cncosta@uc.cl>
2021-07-06 16:47:50 -0400

    Update story_tellingAPI.py

--- story_tellingAPI.py
+++ story_tellingAPI.py
@@ -1,17 +1,25 @@
 import requests
-import random as rd
+from parametros import KEY #key personal
 
-inicio = 'Is The year 2050 an art made by '
-opcion0 = ['humans ', 'artificial inteligence']
-opcion1 = ['Has beccame very popular. ', 'has disappeared. ','has been frobidden. ']
 
-text = str.encode(inicio+rd.choice(opcion0)+rd.choice(opcion1))
-
-r = requests.post(
+def story_telling(enunciado):
+    text = str.encode(enunciado)
+    r = requests.post(
     "https://api.deepai.org/api/text-generator",
     files={
         'text': text,
     },
-    headers={'api-key': 'bb45c840-2ea1-4184-94a6-b903960fd59f'}
-)
-print(r.json()['output'])
+    headers={'api-key':  KEY} #llave personal
+    )
+    return r.json()['output']
+
+
+if __name__ == '__main__':
+    import random as rd
+    
+    inicio = 'Is The year 2050 an art made by '
+    opcion0 = ['humans ', 'artificial inteligence']
+    opcion1 = ['Has beccame very popular. ', 'has disappeared. ','has been frobidden. ']
+    
+    frase  = inicio+rd.choice(opcion0)+rd.choice(opcion1)
+    print(story_telling(frase))
