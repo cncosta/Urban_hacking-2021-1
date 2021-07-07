f0802d30734817c14f736422b64102786099e452
Carlos Costa <cncosta@uc.cl>
2021-07-06 22:21:35 -0400

    Update user_score.py

--- user_score.py
+++ user_score.py
@@ -8,7 +8,7 @@ class User:
     no = 0 # aumenta cada vez que reconoce incorrectamente la diferencia  
     ia = 0 # cantidad de plata que gasta en arte de ia.  
     human = 0 #cantidad de plata que gasta en arte humano
-    def __init__(self):
+    def __init__(self) -> None:
         
         self.u_yes = 0 # aumenta cada vez que reconoce correctamente la diferencia
         self.u_no = 0 # aumenta cada vez que reconoce incorrectamente la diferencia
@@ -16,7 +16,7 @@ class User:
         self.u_ia = 0 # cantidad de plata que gasta en arte de ia.  
         self.u_human = 0 #cantidad de plata que gasta en arte humano.
 
-    def general_result(self):
+    def general_result(self) -> str:
         inicio = 'Is The year 2050, art made by '
         opcion0 = ['humans ', 'artificial inteligence']
         opcion1 = 'is the most popular'
@@ -26,32 +26,22 @@ class User:
             '''
             El usuario reconce en su mayoria el origen de la obra 
             '''
-            if self.ia < self.human: # el usuario valora más el arte humano. 
+            if self.ia <= self.human: # el usuario valora más el arte humano. 
                 return inicio + opcion0[0] + opcion1 + opcion2[0]
             
             elif self.ia > self.human: # el usuario valora más el arte artificial
                 return inicio + opcion0[1] + opcion1 +opcion2[0]
             
-            elif self.ia == self.human: # valora por igual ambos artes
-                pass
-
-    
-        elif self.yes < self.no:
+        elif self.yes <= self.no:
             
-            if self.ia < self.human: # el usuario valora más el arte humano. 
+            if self.ia <= self.human: # el usuario valora más el arte humano. 
                 return inicio + opcion0[0] + opcion1 + opcion2[1]
             
             elif self.ia > self.human: # el usuario valora más el arte artificial
                 return inicio + opcion0[1] + opcion1 +opcion2[1]
             
-            elif self.ia == self.human: # valora por igual ambos artes
-                pass
 
-        elif self.yes == self.no:
-            pass
-
-
-    def user_result(self):
+    def user_result(self)-> str:
         inicio = 'Is The year 2050, art made by '
         opcion0 = ['humans ', 'artificial inteligence']
         opcion1 = 'is the most popular.'
@@ -77,6 +67,10 @@ class User:
                 return inicio + opcion0[0] + opcion1 +opcion2[1]
 
 
+    def __str__(self) -> str:
+        txt = f'Aciertos: {self.u_yes} | monto humanos: {self.u_human} | monto A.I. {self.u_ia} \n'
+        return txt
+
 if __name__ == '__main__':
     import random as rd
     from story_tellingAPI import story_telling
@@ -85,12 +79,9 @@ if __name__ == '__main__':
     for user in range(10):
         usuarios.update({user: User()})
         value_1, value_2 = rd.randint(0,100), rd.randint(0,100) 
-        print(value_2)
         usuarios[user].u_yes = value_1
         usuarios[user].u_no = 100 - value_1
-        usuarios[user].u_ai = value_2
+        usuarios[user].u_ia = value_2
         usuarios[user].u_human = 100 - value_2
-        print(usuarios[user].user_result())
+        print(usuarios[user], story_telling(usuarios[user].user_result()),'\n',40*'_')
         
-
-
